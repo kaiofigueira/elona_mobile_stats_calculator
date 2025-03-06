64a6b30b641173624ea73c143d4359dbb402b023
Kaio Figueira <kaiofigueira@gmail.com>
2025-03-06 11:29:06 -0300

    first commit

---
+++ tipo_stats.py
@@ -0,0 +1,23 @@
+import enum
+
+class TipoStats(enum.Enum):
+    Stats = "Stats"
+    Spd = "Spd"
+    Luck = "Luck"
+    Skills = "Skills"
+    MDR = "MDR"
+    PDR = "PDR"
+    CritChance = "Crit Chance"
+    AddHit = "Add Hit"
+    AddShot = "Add Shot"
+    BonusDmg = "Bonus Dmg"
+    Parry = "Parry"
+    Resistance = "Resistance"
+    MagicPower = "Magic Power"
+    QItemFind = "Q Item Find"
+    SpellTrigger = "Spell Trigger"
+    HealingBonus = "Healing Bonus"
+    MakesBleed = "Makes Bleed"
+    Immunity = "Immunity"
+
+
